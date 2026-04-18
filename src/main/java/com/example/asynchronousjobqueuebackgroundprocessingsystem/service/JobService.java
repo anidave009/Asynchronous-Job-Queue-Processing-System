b7df6a120ab1e2d247e2279408098e0af8f2b9b5
Aniket Dave <daveaniket002@gmail.com>
2026-04-18 18:17:39 +0530

    Implemented Async - app is fast , but limitations are inmemory threds ,scalability , retry

--- src/main/java/com/example/asynchronousjobqueuebackgroundprocessingsystem/service/JobService.java
+++ src/main/java/com/example/asynchronousjobqueuebackgroundprocessingsystem/service/JobService.java
@@ -1,6 +1,7 @@
 package com.example.asynchronousjobqueuebackgroundprocessingsystem.service;
 
 import com.example.asynchronousjobqueuebackgroundprocessingsystem.model.Job;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -10,25 +11,24 @@ import java.util.UUID;
 @Service
 public class JobService {
 
+    private final JobAsyncService jobAsyncService;
+
+    public JobService(JobAsyncService jobAsyncService) {
+        this.jobAsyncService = jobAsyncService;
+    }
+
     public Job submitJob(String jobType){
-        Job job =Job.builder()
+        Job job = Job.builder()
                 .id(UUID.randomUUID().toString())
                 .type(jobType)
                 .status("PENDING")
                 .createdAt(LocalDateTime.now())
                 .build();
-        System.out.println("Starting Job " + job.getId());
-        doHeavyWork(job);
-        System.out.println("Finished Job " + job.getId());
-        return job;
-    }
 
-    private void doHeavyWork(Job job){
-        try{
-            Thread.sleep(5000);
-        }
-        catch(InterruptedException e){
-            Thread.currentThread().interrupt();
-        }
+        System.out.println("Submitting Job " + job.getId());
+
+        jobAsyncService.processJobAsync(job); // async call
+
+        return job;
     }
 }
